f344839af96de2c521892624f4872f9c0123b846
SonnyXD <mamotacris@yahoo.com>
2022-07-03 20:49:41 +0300

    added inventar

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -4,6 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Inventory;
 use Illuminate\Http\Request;
+use \App\Models\Institution;
+use \App\Models\ItemStock;
+use \App\Models\Item;
+use \App\Models\Category;
+use Session;
+use PDF;
+use Auth;
 
 class InventoryController extends Controller
 {
@@ -35,7 +42,104 @@ class InventoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, array(
+            'inventory-select' => 'required'
+        ));
+
+        $user = Auth::user();
+        $institution = Institution::all();
+
+        $now = date('d-m-Y');
+
+        $inventory_id = $request->input('inventory-select');
+
+        $inventory = Inventory::where('id', $inventory_id)->first();
+
+        $categories = Category::all();
+
+        $items = ItemStock::leftjoin('invoice_items', 'invoice_items.id', '=', 'item_stocks.invoice_item_id')
+        ->leftjoin('invoices', 'invoices.id', '=', 'invoice_items.invoice_id')
+        ->leftjoin('measure_units', 'measure_units.id', '=', 'invoice_items.measure_unit_id')
+        ->leftjoin('items', 'items.id', '=', 'invoice_items.item_id')
+        ->where('item_stocks.inventory_id', $inventory->id)
+        ->where('item_stocks.quantity', '!=', 0)
+        ->select(ItemStock::raw('SUM(item_stocks.quantity) as current_quantity'), 'items.name as item_name', 'measure_units.name as um',
+        'invoice_items.lot as lot', 'invoice_items.price as price', 'invoice_items.tva as tva',
+        'invoice_items.tva_price as tva_price', 'invoice_items.exp_date as exp_date', 'items.category_id as category_id')
+        ->groupBy('item_stocks.invoice_item_id')
+        ->get();
+
+        $html = '<html>
+                <head>
+                <style>
+                td, th {border: 1px solid black;}
+                </style>
+                </head>
+                ';
+
+        $html .= ' <span style="font-weight: bold; float: left;">'. $institution[0]->name .'</span>
+                <br>
+                <span style="float: left;">Utilizator: '. $user->name .'</span>
+                <h2 style="font-weight:bold; text-align: center;">INVENTAR</h2>
+                <br>
+                <span style="float: right;">Data: '. $now .'</span>
+                <br>
+                <span style="float: right;">Gestiune: '. $inventory->name .'</span>
+                <br>
+                <br>
+                <br>
+        ';
+
+        $table = '<table>
+            <tr nobr="true">
+            <th style="font-weight: bold; text-align: center;">Denumire Produs</th>
+            <th style="font-weight: bold; text-align: center;">UM</th>
+            <th style="font-weight: bold; text-align: center;">Cantitate</th>
+            <th style="font-weight: bold; text-align: center;">Pret</th>
+            <th style="font-weight: bold; text-align: center;">TVA</th>
+            <th style="font-weight: bold; text-align: center;">Pret TVA</th>
+            <th style="font-weight: bold; text-align: center;">Valoare</th>
+            <th style="font-weight: bold; text-align: center;">Lot</th>
+            <th style="font-weight: bold; text-align: center;">Data expirare</th>
+            </tr>';
+
+            foreach($categories as $category) {
+                $html .= '<span style="font-weight: bold;">'. $category->name .'</span><br><br>';
+                $html .= $table;
+                foreach($items as $item) {
+                    if($item['category_id'] == $category->id) {
+                        $html .= '<tr nobr="true">
+                                        <td style="text-align: center;">'. $item['item_name'] .'</td>
+                                        <td style="text-align: center;">'. $item['um'] .'</td>
+                                        <td style="text-align: center;">'. $item['current_quantity'] .'</td>
+                                        <td style="text-align: center;">'. $item['price'] .'</td>
+                                        <td style="text-align: center;">'. $item['tva'] .'</td>
+                                        <td style="text-align: center;">'. $item['tva_price'] .'</td>
+                                        <td style="text-align: center;">'. $item['current_quantity'] * $item['tva_price'] .'</td>
+                                        <td style="text-align: center;">'. $item['lot'] .'</td>
+                                        <td style="text-align: center;">'. date("d-m-Y", strtotime($item['exp_date'])) .'</td>
+                                    </tr>';
+                    }
+                }
+                $html .= '</table><br><br>';
+            }
+    
+            PDF::setFooterCallback(function($pdf) {
+    
+                // Position at 15 mm from bottom
+                $pdf->SetY(-15);
+                // Set font
+                $pdf->SetFont('helvetica', 'I', 10);
+                // Page number
+                $pdf->Cell(0, 10, 'Pagina '.$pdf->getAliasNumPage().'/'.$pdf->getAliasNbPages(), 0, false, 'C', 0, '', 0, false, 'T', 'M');
+        
+        });
+                
+                PDF::SetTitle('Inventar');
+                PDF::AddPage('L', 'A4');
+                PDF::writeHTML($html, true, false, true, false, '');
+    
+                PDF::Output('name.pdf', 'I');
     }
 
     /**
--- resources/views/documente/inventar.blade.php
+++ resources/views/documente/inventar.blade.php
@@ -1,9 +1,9 @@
 <x-layout>
     <x-container :title="$title">
-        <x-form id="inventar">
+        <x-form id="inventar" method="GET" action="{{ route('inventory.store') }}" target="_blank">
         <div class="form-group">
                     <label>Gestiune:</label>
-                    <select class="form-control">
+                    <select class="form-control" id="inventory-select" name="inventory-select">
                       @if( $inventories->count() )
                         @foreach ($inventories as $inventory)
                                 <option value="{{ $inventory->id }}">{{ $inventory->name }}</option>
